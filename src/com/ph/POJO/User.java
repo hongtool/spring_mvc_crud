775373cfef34b5635130fd7649bb0f21125ce9b4
pan175 <1751265292@qq.com>
2018-07-16 15:50:17 +0800

    数据验证。

--- src/com/ph/POJO/User.java
+++ src/com/ph/POJO/User.java
@@ -1,9 +1,30 @@
 package com.ph.POJO;
 
+import java.util.Date;
+
+import javax.validation.constraints.Past;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.Range;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.NumberFormat;
+
 public class User {
 
 	private String name;
+	
+	@Email
+	private String email;
+	
+	@Range(min = 1, max = 150)
 	private Integer age;
+	
+	@NumberFormat(pattern = "#,###.##")
+	private Double salary;
+	
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	@Past
+	private Date birth;
 
 	public String getName() {
 		return name;
@@ -11,6 +32,14 @@ public class User {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
 	}
 
 	public Integer getAge() {
@@ -21,9 +50,25 @@ public class User {
 		this.age = age;
 	}
 
+	public Double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+
+	public Date getBirth() {
+		return birth;
+	}
+
+	public void setBirth(Date birth) {
+		this.birth = birth;
+	}
+
 	@Override
 	public String toString() {
-		return "User [name=" + name + ", age=" + age + "]";
+		return "User [name=" + name + ", age=" + age + ", salary=" + salary + ", birth=" + birth + "]";
 	}
 
 }
--- src/com/ph/controller/TestHandler.java
+++ src/com/ph/controller/TestHandler.java
@@ -1,6 +1,12 @@
 package com.ph.controller;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.ph.POJO.User;
@@ -12,6 +18,18 @@ public class TestHandler {
 	@RequestMapping("/testConversionService")
 	public String testConversionService(User user) {
 		System.out.println(user);
+		return "success";
+	}
+	
+	@RequestMapping("/testFormat")
+	public String testFormat(@Valid User user, BindingResult result) {
+		System.out.println(user);
+		if(result.getErrorCount() > 0) {
+			List<ObjectError> errors = result.getAllErrors();
+			for(ObjectError error : errors) {
+				System.out.println(error.getDefaultMessage());
+			}
+		}
 		return "success";
 	}
 }
